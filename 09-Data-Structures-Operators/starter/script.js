32f5eb5bdf168a00dcd9dc23f2a3c514cd037eeb
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-01-10 18:05:03 +0100

    feat: complete code challenge 4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,18 +1,28 @@
+'use strict';
+
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 const textarea = document.querySelector('textarea');
 const button = document.querySelector('button');
 
+
+let galochka = ' ';
 function convertUnderToUpper(str) {
   const rows = str.split('\n');
+  let arr = []; 
+  rows.forEach(element => arr.push(element.length));
+  let maxLength = Math.max(...arr);
   for(let row of rows) {
+    galochka += '✅';
     const [first, second] =  row.toLowerCase().trim().split('_');
-    return `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    const result = first + second.replace(second[0], second[0].toUpperCase());
+    console.log(result.padEnd(maxLength+ galochka);
   }
 }
+
 function transform() {
-  const text = textarea.innerText;
-  console.log(convertUnderToUpper(text));
+  const text = textarea.value;
+  convertUnderToUpper(text);
 }
 
 button.addEventListener('click', transform);
