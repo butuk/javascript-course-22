487d33f644299a086af63900d1c8b35068624202
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-01-10 12:31:07 +0100

    feat: add function into function example

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -2,3 +2,13 @@
 
 const addTax = (rate, value) => value + rate * value;
 console.log(addTax(0.1, 200));
+
+function addTax2(rate) {
+  return function(value) {
+    return value + value * rate;
+  }
+};
+
+const addVAT = addTax2(0.23);
+
+console.log(addVAT(100));
